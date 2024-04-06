c40abef5192f1a574e51fc1919227e644b2434ea
sudo <sudo@gmail.com>
2024-04-06 20:38:24 +0500

    add login and register view

--- student/urls.py
+++ student/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import StudentListView, LandingView
+from .views import StudentListView, LandingView, UserRegisterView, UserLoginView
 
 urlpatterns = [
     path("student/", StudentListView.as_view(), name="student"),
     path("", LandingView.as_view(), name="landing"),
+    path("auth/register/", UserRegisterView.as_view(), name="register"),
+    path("auth/login/", UserLoginView.as_view(), name="login"),
 ]
